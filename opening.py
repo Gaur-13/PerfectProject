54f136db78e9584ef97fcbb51048744dc34596f0
Gaur-13 <pochtadlyekfi@gmail.com>
2026-01-18 11:45:15 +0300

    Update opening.py
    Add new defs

--- opening.py
+++ opening.py
@@ -8,10 +8,29 @@ def open(d):
     return t
 
 doc = Document()
+rights = []
 
-def writein(str):
+def add(str):
     global doc
     doc.add_paragrath(str)
 
+def writerights():
+    global rights
+    global doc
+    varnow = rights[0][0]
+    answers = ""
+    for list in rights:
+        if list[0] != varnow:
+            varnow = list[0]
+            answers += "Вариант№"+varnow+" "
+        answers += list[1]+list[2]
+    doc.add_paragrath(answers)
+
+def addright(var, qes, ans):
+    global rights
+    rights.upend([var, qes+" - ", qns+" "])
+
 def savedoc():
+    global doc
+    writerights()
     doc.save("test.docx")
